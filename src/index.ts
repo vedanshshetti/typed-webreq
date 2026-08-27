868d47f59c807194a782f2dbf093d1d39e999e59
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-08-27 21:14:57 +0200

    v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { isNullish, mapStatus } from "./utils";
 
 
 const buildURI = (protocol: RequestProtocol, URIPath: string): string => `${protocol}${URIPath}`;
-const createError = (message: string, uri: string) => new Error(`[@typescript-utils/typed-webreq - ${new Date().toISOString()}]: An error occured while attempting to fetch data from ${uri}: ${message}`);
+const createError = (message: string, uri: string) => (`[@typescript-utils/typed-webreq - ${new Date().toISOString().split(".")[0]}]: An error occured while attempting to fetch data from ${uri}: ${message}`);
 
 interface WebReqMetadataInterface {
     method?: ReqMethod;
@@ -31,7 +31,7 @@ interface WebReqSuccessfulReturnInterface {
 
 export default async function webreq(metadata: WebReqMetadataInterface): Promise<WebReqSuccessfulReturnInterface> {
     const reqURI = buildURI(metadata.api.protocol, `${metadata.api.domain ?? ""}${metadata.api.path}`);
-    if(metadata.api.protocol.startsWith("http") && isNullish(metadata.api.domain)) throw createError(`HTTP / HTTPS requests always require a domain.`, reqURI);
+    if(metadata.api.protocol.startsWith("http") && isNullish(metadata.api.domain)) throw new Error(createError(`HTTP / HTTPS requests always require a domain.`, reqURI));
     const schema = metadata.returnValue ? tsv.defineSchema(`${metadata.method} ${reqURI}`, metadata.returnValue, "english") : null;
     let req;
     try {
@@ -40,7 +40,7 @@ export default async function webreq(metadata: WebReqMetadataInterface): Promise
         method: metadata.method ?? "GET"
         });
     } catch(e) {
-        throw createError("Following error occured while attempting request: "+JSON.stringify(e), reqURI);
+        throw new Error(createError("Following error occured while attempting request: "+JSON.stringify(e), reqURI));
     }
     const contentType = req.headers.get("content-type");
     let jsonBody: JsonV | null = null;
@@ -50,15 +50,15 @@ export default async function webreq(metadata: WebReqMetadataInterface): Promise
     } catch {
       jsonBody = null;
     }
-    if(!req.ok) throw createError(`Server${!metadata.api.domain ? "" : " at "+metadata.api.domain} returned HTTP Status "${req.status} - ${mapStatus(req.status.toString())}"`, reqURI);
+    if(!req.ok) throw new Error(createError(`Server${!metadata.api.domain ? "" : " at "+metadata.api.domain} returned HTTP Status "${req.status} - ${mapStatus(req.status.toString())}"`, reqURI));
     if(contentType?.includes("application/json") && isNullish(jsonBody)) throw createError(`Header "Content-Type" is "application/json" but JSON Body is nonexistent.`, reqURI);
     if (contentType?.includes("application/json")) {
         const validation = schema!.validate(jsonBody as JsonV);
         if (!validation.valid) {
-              throw createError(validation.errMessage ?? "Data doesn't match expected return type.", reqURI);
+              throw new Error(createError(validation.errMessage ?? "Data doesn't match expected return type.", reqURI));
         }
     } else {
-        throw createError(`Unsupported Content-Type: ${contentType ?? "missing"}`, reqURI);
+        throw new Error(createError(`Unsupported Content-Type: ${contentType ?? "missing"}`, reqURI));
     }
 
     return {
